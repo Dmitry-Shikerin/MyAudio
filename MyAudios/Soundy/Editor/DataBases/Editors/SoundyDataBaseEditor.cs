871483a9e04ece764a76b7dc313e9953c0180417
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-07-07 21:24:09 +0300

    add

--- MyAudios/Soundy/Editor/DataBases/Editors/SoundyDataBaseEditor.cs
+++ MyAudios/Soundy/Editor/DataBases/Editors/SoundyDataBaseEditor.cs
@@ -1,9 +1,20 @@
-﻿using UnityEditor;
+﻿using Doozy.Engine.Soundy;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace MyAudios.Soundy.Editor.DataBases.Editors
 {
+    [CustomEditor(typeof(SoundyDatabase))]
     public class SoundyDataBaseEditor : UnityEditor.Editor
     {
+        public VisualTreeAsset VisualTreeAsset;
         
+        public override VisualElement CreateInspectorGUI()
+        {
+            VisualElement root = new VisualElement();
+            VisualTreeAsset.CloneTree(root);
+
+            return root;
+        }
     }
 }
--- MyAudios/Soundy/Editor/DataBases/Windows/SoundyDataBaseEditorWindow.cs
+++ MyAudios/Soundy/Editor/DataBases/Windows/SoundyDataBaseEditorWindow.cs
@@ -1,9 +1,47 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using Doozy.Engine.Soundy;
+using UnityEditor;
+using UnityEditorInternal.VersionControl;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace MyAudios.Soundy.Editor.DataBases.Windows
 {
     public class SoundyDataBaseEditorWindow : EditorWindow
     {
-        
+        public VisualTreeAsset VisualTreeAsset;
+        public SoundyDatabase Database;
+
+        [MenuItem("Window/SoundyDataBase")]
+        public static void ShowWindow()
+        {
+            GetWindow<SoundyDataBaseEditorWindow>("Soundy Data Base");
+        }
+
+        public void CreateGUI()
+        {
+            VisualElement root = rootVisualElement;
+            VisualTreeAsset.CloneTree(root);
+
+            // root.Q<ListView>("SoundDataBases").itemsSource = Database.SoundDatabases;
+            ListView listView = root.Q<ListView>("SoundDataBases");
+            listView.itemsSource = Database.DatabaseNames;
+            listView.selectionChanged += OnItemSelekted;
+        }
+
+        protected virtual void OnGUI()
+        {
+            // VisualElement root = new VisualElement();
+            // VisualTreeAsset.CloneTree(root);
+        }
+
+        private void OnItemSelekted(IEnumerable<object> obj)
+        {
+            foreach (object item in obj)
+            {
+                Debug.Log($"Item");
+            }
+        }
     }
 }
